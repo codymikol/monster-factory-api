1e02187bff8529f9e490bd06936b00dd72185c62
Cody Mikol <codymikol@gmail.com>
2023-11-08 14:54:35 -0500

    fix(build): set jvm target
    ...
    
    Fixes N/A

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,8 @@ repositories {
     mavenCentral()
 }
 
+java.sourceCompatibility = org.gradle.api.JavaVersion.VERSION_17
+
 dependencies {
     implementation("io.arrow-kt:arrow-core:1.2.0")
     implementation("io.arrow-kt:arrow-fx-coroutines:1.2.0")
